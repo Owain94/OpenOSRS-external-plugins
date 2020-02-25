e7b66a3e78e469e5510cf5b187ff2036c8d968b4
Owain van Brakel <owain.vanbrakel@gmail.com>
2020-02-26 00:08:53 +0100

    plugin: Ignore compliance plugin

---
+++ ignorecompliance/src/main/kotlin/com/owain/ignorecompliance/CompliancePlugin.kt
@@ -0,0 +1,29 @@
+package com.owain.chinglassblow
+
+import net.runelite.api.Client
+import net.runelite.client.plugins.Plugin
+import net.runelite.client.plugins.PluginDescriptor
+import net.runelite.client.plugins.PluginType
+import org.pf4j.Extension
+import javax.inject.Inject
+
+@Extension
+@PluginDescriptor(
+        name = "Ignore compliance",
+        description = "Sets the compliance value of OpenOSRS to false",
+        type = PluginType.MISCELLANEOUS
+)
+class CompliancePlugin : Plugin() {
+
+    @Inject
+    private lateinit var client: Client
+
+    override fun startUp() {
+        client.setComplianceValue("orbInteraction", true)
+    }
+
+
+    override fun shutDown() {
+        client.setComplianceValue("orbInteraction", false)
+    }
+}

bc7a598253662b6354376b2bb5b5099245ddc03f
Owain van Brakel <owain.vanbrakel@gmail.com>
2020-03-10 14:07:51 +0100

    project: Update dependencies

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -30,19 +30,17 @@ object ProjectVersions {
 
 object Libraries {
     private object Versions {
-        const val apacheCommonsLang = "3.9"
+        const val apacheCommonsText = "1.8"
         const val guice = "4.2.2"
         const val javax = "1.3.2"
-        const val lombok = "1.18.10"
+        const val lombok = "1.18.12"
         const val pf4j = "3.2.0"
-        const val slf4j = "1.7.30"
     }
 
-    const val apacheCommonsLang = "org.apache.commons:commons-lang3:${Versions.apacheCommonsLang}"
+    const val apacheCommonsText = "org.apache.commons:commons-text:${Versions.apacheCommonsText}"
     const val guice = "com.google.inject:guice:${Versions.guice}:no_aop"
     const val javax = "javax.annotation:javax.annotation-api:${Versions.javax}"
     const val lombok = "org.projectlombok:lombok:${Versions.lombok}"
     const val pf4j = "org.pf4j:pf4j:${Versions.pf4j}"
-    const val slf4j = "org.slf4j:slf4j-api:${Versions.slf4j}"
 
 }

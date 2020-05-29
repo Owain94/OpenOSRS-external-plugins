4dc6c39b1cdf2c63d8c476e86663df2ea5175b27
OpenOSRS <openosrs.github@gmail.com>
2020-05-29 00:03:27 +0000

    project: Update gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     kotlin("jvm") version "1.3.71"
     id("com.simonharrer.modernizer") version "1.8.0-1" apply false
     id("com.github.ben-manes.versions") version "0.28.0"
-    id("se.patrikerdes.use-latest-versions") version "0.2.13"
+    id("se.patrikerdes.use-latest-versions") version "0.2.14"
 }
 
 apply<BootstrapPlugin>()

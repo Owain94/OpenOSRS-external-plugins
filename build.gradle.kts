213878dfe0812256f471c0b96105ff71125d31f0
OpenOSRS <openosrs.github@gmail.com>
2020-07-23 00:04:23 +0000

    project: Update gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     java
     kotlin("jvm") version "1.3.71"
     id("com.simonharrer.modernizer") version "2.1.0-1" apply false
-    id("com.github.ben-manes.versions") version "0.28.0"
+    id("com.github.ben-manes.versions") version "0.29.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.14"
 }
 
@@ -61,7 +61,7 @@ subprojects {
         compileOnly(group = "org.apache.commons", name = "commons-text", version = "1.8")
         compileOnly(group = "com.google.inject", name = "guice", version = "4.2.3", classifier = "no_aop")
         compileOnly(group = "org.projectlombok", name = "lombok", version = "1.18.12")
-        compileOnly(group = "org.pf4j", name = "pf4j", version = "3.3.1")
+        compileOnly(group = "org.pf4j", name = "pf4j", version = "3.4.0")
 
         // kotlin
         compileOnly(kotlin("stdlib"))

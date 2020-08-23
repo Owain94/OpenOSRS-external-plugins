e8fef45fb2e6fdb15b15c56bdb94b9264da1ec91
Owain van Brakel <owain.vanbrakel@gmail.com>
2020-08-23 07:32:22 +0200

    chinbreakhandler: Resume after AFK

--- chinbreakhandler/chinbreakhandler.gradle.kts
+++ chinbreakhandler/chinbreakhandler.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.11"
+version = "0.0.12"
 
 project.extra["PluginName"] = "Chin break handler"
 project.extra["PluginDescription"] = "Automatically takes breaks for you (?)"

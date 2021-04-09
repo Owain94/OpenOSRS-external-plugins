d73c1669c87c62f0fdafccdd74009a44096ad38d
Owain van Brakel <owain.vanbrakel@gmail.com>
2021-04-10 01:52:31 +0200

    chinbreakhandler: Checkstyle

--- chinbreakhandler/src/main/java/com/owain/chinbreakhandler/ChinBreakHandler.java
+++ chinbreakhandler/src/main/java/com/owain/chinbreakhandler/ChinBreakHandler.java
@@ -13,7 +13,6 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import net.runelite.client.config.ConfigManager;
